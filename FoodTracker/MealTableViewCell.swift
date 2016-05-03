6b33fde0c30699b80c08a0a717059489f3468d65
danny.p <danny.p@kakaocorp.com>
2016-05-03 15:31:30 +0900

    Create a Table View

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by danny.p on 2016. 5. 3..
+//  Copyright © 2016년 dhen-p. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    // MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
